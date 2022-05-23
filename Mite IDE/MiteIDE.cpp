9569c23ec4a9cde5f14216e1ffa91d8a0a6cbeb6
Matthew Harris <matt.harris2908@gmail.com>
2022-05-23 19:23:06 +1000

    basic echo console

---
+++ Mite IDE/MiteIDE.cpp
@@ -0,0 +1,13 @@
+#include "common.h"
+
+int main(int argc, char** argv)
+{
+	bool exit = false;
+
+	while (!exit)
+	{
+		std::cout << std::cin.get();
+	}
+
+	return 0;
+}
--- Mite IDE/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "common.h"
-
-int main(int argc, char** argv)
-{
-	std::cout << "Mite IDE" << std::endl;
-
-	return 0;
-}

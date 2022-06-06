f4acb0616c7e62fe53c334f0d355c26e8f5118e6
Matthew Harris <matt.harris2908@gmail.com>
2022-06-06 18:40:27 +1000

    eh

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { runCli } from './cli/interface.ts';
 
 
+function recTest(fromRec: boolean) {
+    if (!recTest) {
+        recTest(true);
+        console.log('1')
+    } else {
+        console.log('2')
+    }
+}
+
+recTest(false);
 
 // running the cli is the last thing to do
 runCli();

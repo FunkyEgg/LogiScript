123f8e2c1056e5f59f3b48f4f7b50c2d4f231e49
Matthew Harris <matt.harris2908@gmail.com>
2022-06-09 18:37:52 +1000

    started work on a better analyzer

--- src/lang/analyzer.ts
+++ src/lang/analyzer.ts
@@ -5,25 +5,61 @@ export function analyze(tokens: string[]): void {
 
     let i = 0;
 
-    for (let _i in tokens) {
-        if (skipIter) {
-            skipIter = false;
-            continue;
-        }
+    // for (let _i in tokens) {
+    //     if (skipIter) {
+    //         skipIter = false;
+    //         continue;
+    //     }
         
+    //     let token = tokens[i];
+
+    //     switch (token) {
+    //         case 'gate' || 'comp': {
+    //             if (parseInt(tokens[i + 2]) <= 0) {
+    //                 console.log(`Can't asign ${tokens[i + 2]} to a number of inputs`);
+    //                 Deno.exit(-1)
+    //             }
+    //             break;
+    //         }
+
+    //         default: {
+    //             console.log(`${token} Is a invalid token`);
+    //             Deno.exit(-1);
+    //         }
+    //     }
+
+    //     i++;
+    // }
+
+    while (i < tokens.length) {
         let token = tokens[i];
 
         switch (token) {
             case 'gate' || 'comp': {
-                if (parseInt(tokens[i + 2]) <= 0) {
-                    console.log(`Can't asign ${tokens[i + 2]} to a number of inputs`);
+                if (parseInt(tokens[i+2]) <= 0) {
+                    console.log(`Can't asign ${tokens[i+2]} to a number of inputs`);
                     Deno.exit(-1)
                 }
+
+                let numOfInputs = tokens[i+2];
+                let i2 = i+2;
+
+                while (i2 < tokens.length) {
+                    let token = tokens[i2]
+
+                    switch (token) {
+                        default: {
+                            console.log(`${token} is not a valid token in this context`);
+                            Deno.exit(-1)
+                        }
+                    }
+                }
+
                 break;
             }
 
             default: {
-                console.log(`${token} Is a invalid token`);
+                console.log(`You must start by declaring`);
                 Deno.exit(-1);
             }
         }

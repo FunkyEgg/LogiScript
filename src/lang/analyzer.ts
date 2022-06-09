410177ebcece56ea7af0122aaf9e615fedcd0368
Matthew Harris <matt.harris2908@gmail.com>
2022-06-09 18:38:12 +1000

    removed massive for loop comment

--- src/lang/analyzer.ts
+++ src/lang/analyzer.ts
@@ -5,32 +5,6 @@ export function analyze(tokens: string[]): void {
 
     let i = 0;
 
-    // for (let _i in tokens) {
-    //     if (skipIter) {
-    //         skipIter = false;
-    //         continue;
-    //     }
-        
-    //     let token = tokens[i];
-
-    //     switch (token) {
-    //         case 'gate' || 'comp': {
-    //             if (parseInt(tokens[i + 2]) <= 0) {
-    //                 console.log(`Can't asign ${tokens[i + 2]} to a number of inputs`);
-    //                 Deno.exit(-1)
-    //             }
-    //             break;
-    //         }
-
-    //         default: {
-    //             console.log(`${token} Is a invalid token`);
-    //             Deno.exit(-1);
-    //         }
-    //     }
-
-    //     i++;
-    // }
-
     while (i < tokens.length) {
         let token = tokens[i];
 

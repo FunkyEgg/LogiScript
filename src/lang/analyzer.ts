f492f7afacd18095c30d282d5c80496c13f3daa5
Matthew Harris <matt.harris2908@gmail.com>
2022-06-09 18:45:53 +1000

    Started work on recursive analysis

--- src/lang/analyzer.ts
+++ src/lang/analyzer.ts
@@ -9,25 +9,24 @@ export function analyze(tokens: string[]): void {
         let token = tokens[i];
 
         switch (token) {
-            case 'gate' || 'comp': {
+            case 'gate': {
                 if (parseInt(tokens[i+2]) <= 0) {
                     console.log(`Can't asign ${tokens[i+2]} to a number of inputs`);
-                    Deno.exit(-1)
+                    Deno.exit(-1);
                 }
 
-                let numOfInputs = tokens[i+2];
-                let i2 = i+2;
+                recAnalyzeGate(tokens, i, parseInt(tokens[i+2]));
+                
+                break;
+            }
 
-                while (i2 < tokens.length) {
-                    let token = tokens[i2]
-
-                    switch (token) {
-                        default: {
-                            console.log(`${token} is not a valid token in this context`);
-                            Deno.exit(-1)
-                        }
-                    }
+            case 'comp': {
+                if (parseInt(tokens[i+2]) <= 0) {
+                    console.log(`Can't asign ${tokens[i+2]} to a number of inputs`);
+                    Deno.exit(-1);
                 }
+
+                recAnalyzeComp(tokens, i, parseInt(tokens[i+2]));
 
                 break;
             }
@@ -39,5 +38,45 @@ export function analyze(tokens: string[]): void {
         }
 
         i++;
+    }
+}
+
+// recursive analyze function for a gate
+function recAnalyzeGate(tokens: string[], iter: number, numOfInputs: number) {
+    let i = iter+2;
+
+    while (i < tokens.length) {
+        let token = tokens[i];
+
+        // TODO
+
+        switch (token) {
+            case 'not': {
+
+            }
+
+            default: {
+                console.log(`${token} is not a valid token in this context`);
+                Deno.exit(-1);
+            }
+        }
+    }
+}
+
+// recursive analyze function for a component
+function recAnalyzeComp(tokens: string[], iter: number, numOfInputs: number) {
+    let i = iter+2;
+
+    while (i < tokens.length) {
+        let token = tokens[i];
+
+        // TODO
+
+        switch (token) {
+            default: {
+                console.log(`${token} is not a valid token in this context`);
+                Deno.exit(-1);
+            }
+        }
     }
 }
